a4d86831afb053c63c5e2b480bff57730f1bf441
Eimutisg <eimutis.genys@gmail.com>
2017-10-20 16:59:38 +0100

    Start of Project
    Forst Commit

---
+++ app/src/main/java/com/example/mano/quicklauncher/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.mano.quicklauncher;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Second Activity Button
+        Button secondActivityBtn = (Button) findViewById(R.id.secondActivityBtn);
+        secondActivityBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent startIntent = new Intent(getApplicationContext(), SecondActivity.class);
+
+                EditText userInput = (EditText) findViewById(R.id.userInputEditText);
+                String userIn = userInput.getText().toString();
+
+                startIntent.putExtra("com.example.quicklauncher.SOMETHING", userIn);
+
+                startActivity(startIntent);
+            }
+        });
+
+        // Google Button - Launch activity outside our app
+        Button googleBtn = (Button) findViewById(R.id.googleBtn);
+        googleBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String google = "https://www.yahoo.ie";
+                Uri webAddress = Uri.parse(google);
+
+                Intent gotoGoogle = new Intent(Intent.ACTION_VIEW, webAddress);
+                if(gotoGoogle.resolveActivity(getPackageManager()) != null) {
+                    startActivity(gotoGoogle);
+                }
+            }
+        });
+
+    }
+}
